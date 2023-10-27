269e8bc2969fcc72562d472cce1c718163ec756d
HoangShiro <hoangdao13999@gmail.com>
2023-10-27 14:56:06 +0700

    Test

--- bot.py
+++ bot.py
@@ -1126,4 +1126,6 @@ def bot_run():
     bot.run(discord_bot_key)
 
 if __name__ == '__main__':
-    bot_run()
+    loop = asyncio.get_event_loop()
+    loop.create_task(bot_run())
+    loop.run_forever()

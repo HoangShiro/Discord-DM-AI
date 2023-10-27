8ec8ec7a7bddf91f96990bc74c3df41c66477b1c
HoangShiro <hoangdao13999@gmail.com>
2023-10-27 14:52:33 +0700

    test

--- main.py
+++ main.py
@@ -4,9 +4,19 @@ import subprocess
 github_repo = "https://github.com/HoangShiro/Discord-DM-AI.git"
 
 def update_bot():
-    try:  
+    try:
+        # Kiểm tra xem có thay đổi cục bộ không
+        status = subprocess.run(["git", "status", "--porcelain"], stdout=subprocess.PIPE, text=True)
+        if status.stdout:
+            # Nếu có thay đổi cục bộ, hãy tạm giữ chúng
+            subprocess.run(["git", "stash"])
+        
         # Sử dụng Git để cập nhật mã nguồn
         subprocess.run(["git", "pull", "--no-edit", "--no-rebase"])
+        
+        # Khôi phục các thay đổi cục bộ (nếu có)
+        if status.stdout:
+            subprocess.run(["git", "stash", "pop"])
         
         print("Bot đã cập nhật thành công từ GitHub.")
     except Exception as e:

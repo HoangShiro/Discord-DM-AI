b09563d1f08ca1cbbaedfcbe13ad1e46fe309e18
HoangShiro <hoangdao13999@gmail.com>
2023-10-30 01:05:06 +0700

    Update

--- utils/tts.py
+++ utils/tts.py
@@ -36,7 +36,7 @@ def tts_get_en(tts, speaker):
                     sample_rate=sample_rate,
                 filename='user_files/test.wav')
     
-    input_wav_file = 'test.wav'
+    input_wav_file = 'user_files/test.wav'
     output_ogg_file = 'user_files/ai_voice_msg.ogg'
     audio = AudioSegment.from_wav(input_wav_file)
     audio.export(output_ogg_file, format="ogg")

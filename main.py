ae2bb4317d1c0d50d3119043983110f060f6aa80
HoangShiro <hoangdao13999@gmail.com>
2023-11-06 01:15:22 +0700

    Update

--- main.py
+++ main.py
@@ -1,21 +1,17 @@
 import subprocess
-import os
 
 github_repo = "https://github.com/HoangShiro/Discord-DM-AI.git"
 
 def update_bot():
-    if not os.path.exists(".git"):
-        # Thư mục Git chưa tồn tại, nên ta sẽ clone dự án từ GitHub
-        subprocess.run(["git", "clone", github_repo, "."], check=True)
-    else:
-        try:
-            # Đảm bảo bạn đang ở trạng thái sạch, không có sự thay đổi cục bộ
-            subprocess.run(["git", "reset", "--hard", "HEAD"], check=True)
+    try:
+        # Đảm bảo bạn đang ở trạng thái sạch, không có sự thay đổi cục bộ
+        subprocess.run(["git", "reset", "--hard", "HEAD"], check=True)
 
-            # Lấy phiên bản mới nhất từ kho GitHub
-            subprocess.run(["git", "pull", "origin", "main"], check=True)
-        except Exception as e:
-            print(f"Lỗi khi cập nhật bot từ GitHub: {e}")
+        # Lấy phiên bản mới nhất từ kho GitHub
+        subprocess.run(["git", "pull", "origin", "main"], check=True)
+        
+    except Exception as e:
+        print(f"Lỗi khi cập nhật bot từ GitHub: {e}")
 
 def start():
     import utils.bot as bot

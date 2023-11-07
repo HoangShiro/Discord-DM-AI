60468d6943bacd8ab9a91cc8404d092f6a048b7b
HoangShiro <hoangdao13999@gmail.com>
2023-11-07 07:35:16 +0700

    Fix

--- utils/bot.py
+++ utils/bot.py
@@ -660,7 +660,7 @@ async def image_search(interaction: discord.Interaction, keywords: str, limit: i
             return
         embed = discord.Embed(description=f"{keywords}", color=discord.Color.blue())
         for img_url in img_urls:
-            embed.add_field(name='image', value=img_url, inline=False)
+            embed.set_image(url=img_url)
         view = View()
         view.add_item(irmv_bt)
         await interaction.response.send_message(embed=embed, view=view)

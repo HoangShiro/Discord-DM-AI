bc00d01925458298ac69a665ee4139ba1e855293
HoangShiro <hoangdao13999@gmail.com>
2023-11-09 02:54:39 +0700

    Update

--- utils/bot.py
+++ utils/bot.py
@@ -713,11 +713,11 @@ async def image_search(interaction: discord.Interaction, keywords: str, limit: i
 
         async def update_embed(interaction, index, img_url, num, tags):
         # Tạo một Embed mới với URL hình ảnh mới từ img_urls
-            new_embed = discord.Embed(description=f"🏷️ {tags}\n🎨 {index+1}/{num}\n💟 {img_url['rating']}", color=discord.Color.blue())
+            new_embed = discord.Embed(description=f"🏷️ {tags}\n🎨 {index+1}/{num}\n💟 {img_url['rating']}", url=url, color=discord.Color.blue())
             new_embed.set_image(url=img_url['file_url'])
             url = img_url['file_url']
             if url.endswith((".mp4", ".webp")):
-                await interaction.response.edit_message(embed=new_embed, url=url, view=view)
+                await interaction.response.edit_message(embed=new_embed, view=view)
             else:
                 await interaction.response.edit_message(content=None, embed=new_embed, view=view)
 

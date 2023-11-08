f4d3ab03ab3de88a2c29d27f6e032d9034446d7e
HoangShiro <hoangdao13999@gmail.com>
2023-11-09 02:50:27 +0700

    Test

--- utils/bot.py
+++ utils/bot.py
@@ -708,16 +708,16 @@ async def image_search(interaction: discord.Interaction, keywords: str, limit: i
             await interaction.response.send_message(f"Không có art nào với '{keywords}'", ephemeral=True)
             return
 
-        embed = discord.Embed(description=f"🔡 {fix_kws}\n🎨 {index+1}/?\n💟 {imgs[index]['rating']}", color=discord.Color.blue())
+        embed = discord.Embed(description=f"🏷️ {fix_kws}\n🎨 {index+1}/?\n💟 {imgs[index]['rating']}", color=discord.Color.blue())
         embed.set_image(url=imgs[0]['file_url'])
 
         async def update_embed(interaction, index, img_url, num, tags):
         # Tạo một Embed mới với URL hình ảnh mới từ img_urls
-            new_embed = discord.Embed(description=f"🔡 {tags}\n🎨 {index+1}/{num}\n💟 {img_url['rating']}", color=discord.Color.blue())
+            new_embed = discord.Embed(description=f"🏷️ {tags}\n🎨 {index+1}/{num}\n💟 {img_url['rating']}", color=discord.Color.blue())
             new_embed.set_image(url=img_url['file_url'])
             url = img_url['file_url']
             if url.endswith((".mp4", ".webp")):
-                await interaction.response.edit_message(content=f"🔡 {tags}\n🎨 {index+1}/{num}\n💟 {img_url['rating']}\n🔗 {url}", embed=None, view=view)
+                await interaction.response.edit_message(embed=new_embed, content=url, view=view)
             else:
                 await interaction.response.edit_message(content=None, embed=new_embed, view=view)
 
